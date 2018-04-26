308b3fa8ada9a4155801320554f097002997d571
jrodriguez88 <jeferson_88@hotmail.com>
2018-04-26 17:16:52 -0500

    Add MAster and Weather Function

---
+++ MASTER.R
@@ -0,0 +1,37 @@
+#########################################################
+####            Master ORYZA_Model_RTOOLS            ####
+####     By https://github.com/jrodriguez88          ####
+####      ORYZA Rice Crop Model described in:        #### 
+####   http://books.irri.org/9712201716_content.pdf  ####
+#########################################################
+
+
+##### LOCAL DATA
+
+local <- "AIHU"
+lat <- 3.253
+lon <- -75.24
+alt <- 380
+
+read_INPUT_data <- function(filename) {
+    sheets <- readxl::excel_sheets(filename)
+    x <-    lapply(sheets, function(X) readxl::read_excel(filename, sheet = X))
+    names(x) <- sheets
+    x
+}
+
+################################
+#### Load Requeriments      ####
+################################
+
+if(require(lubridate)==FALSE){install.packages("lubridate")}
+if(require(readxl)==FALSE){install.packages("readxl")}
+if(require(RCurl)==FALSE){install.packages("RCurl")}
+if(require(tidyverse)==FALSE){install.packages("tidyverse")}
+if(require(stringr)==FALSE){install.packages("stringr")}
+if(require(magrittr)==FALSE){install.packages("magrittr")}
+if(require(data.table)==FALSE){install.packages("data.table")}
+if(require(plyr)==FALSE){install.packages("plyr")}
+
+
+
--- Make_WTH_ORYZA.R
+++ Make_WTH_ORYZA.R
@@ -1,80 +1,72 @@
-
-# __    __           _   _   _                  __ _ _              
-#/ / /\ \ \___  __ _| |_| |_| |__   ___ _ __   / _(_) | ___         
-#\ \/  \/ / _ \/ _` | __| __| '_ \ / _ \ '__| | |_| | |/ _ \  _____ 
-# \  /\  /  __/ (_| | |_| |_| | | |  __/ |    |  _| | |  __/ |_____|
-#  \/  \/ \___|\__,_|\__|\__|_| |_|\___|_|    |_| |_|_|\___|        
-
-#   ___                     ____   ___   ___   ___  
-#  /___\_ __ _   _ ______ _|___ \ / _ \ / _ \ / _ \ 
-# //  // '__| | | |_  / _` | __) | | | | | | | | | |
-#/ \_//| |  | |_| |/ / (_| |/ __/| |_| | |_| | |_| |
-#\___/ |_|   \__, /___\__,_|_____|\___/ \___/ \___/ 
-#            |___/                                  
-
 ######################################################################################
-####### Crea la estructura de las extenciones de los archivos climáticos #############
+####### Crea la estructura de las extenciones de los archivos climaticos #############
 ####################    by JRE- https://github.com/jrodriguez88    ###################     
 ######################################################################################
+#file <- "AIHU_FED2000.xlsx"
 
-#Cargar Packages
-require(stringr)
+#  Column    Daily Value                                                                         
+#     1      Station number                                                                       
+#     2      Year                                                                           
+#     3      Day                                                                    
+#     4      irradiance         KJ m-2 d-1                                            
+#     5      min temperature            oC                                                        
+#     6      max temperature            oC                                                        
+#     7      vapor pressure            kPa                                                        
+#     8      mean wind speed         m s-1                                                        
+#     9      precipitation          mm d-1
 
-###Defina datos de analisis
-user <- "C:\\ORYZA\\WEATHER\\"       #Definir nombre de usuario/path en windows
-setwd(user)
-loc <- "VVME"                        #Definir nombre de localidad, 4 letras
-cord <- "-72.53,4.03,315,0,0"        #Definir coordenadas de localidad, ver info abajo
-
-#Modificar coordenadas (solo info)
-#SDTO -74.98,3.917,305,0,0
-#AIHU -74.767,3.250,380,0,0
-#MRCO -74.15,8.8,14,0,0
-#YOCS -72.15,5.15,250,0,0
-#VVME -72.53,4.03,315,0,0
-#IBTO -75.087, 4.42, 750,0,0
+DATA <- read.csv(paste0(getwd(),"//", "weather_input.csv"),header = T) %>%
+        mutate(Station_number = 1,
+           Year = year(DATE),
+           Day = yday(DATE),
+           SRAD = round(SRAD, 2),
+           TMAX = round(TMAX, 2),
+           TMIN = round(TMIN, 2),
+           RAIN = round(RAIN, 2),
+           VPD = -99, 
+           WS = -99) %>%
+    select(Station_number, Year, Day, SRAD, TMIN, TMAX, VPD, WS, RAIN)
 
 
-#Definir años de analisis
-year_i=2008
-year_f=2016
+Make_WTH_ORYZA <- function(DATA) {
+    
+dir.create(paste0(getwd(),"/WTH"), showWarnings = FALSE)
+set_head <- paste(LON, LAT, ALT, 0, 0, sep = ",")    
+#DATA=read.table(file, head=T)  
+year_i=min(DATA$Year)
+year_f=max(DATA$Year)
 
-
-#Cargar Datos copiados, Formato: "cod	ano	dia	rad	tmin tmax viento vapor lluvia"
-DATA=read.table("clipboard",head=T) 
 
 #Crea matrix para orgnizar datos
-ano=cbind(year_i:year_f)
-ano=as.data.frame(ano)
+year=cbind(year_i:year_f)
+year=as.data.frame(year)
 
-fechas_clima=str_sub(ano[1,1],-3,-1)
+dates_wth=str_sub(year[1,1],-3,-1)
 
-for (z in 2:nrow(ano)){
-  fechas_clima=rbind(fechas_clima,(str_sub(ano[z,1],-3,-1)))  
+for (z in 2:nrow(year)){
+    dates_wth=rbind(dates_wth,(str_sub(year[z,1],-3,-1)))  
 }
 
-fechas_clima=as.data.frame(fechas_clima)
+dates_wth <- as.data.frame(dates_wth)
 
+wth_year=list()
 
-
-####################################################################################
-######### Cre los archivos climáticos para el modelo Oryza2000, por cada año #######
-####################################################################################
-
-
-clima_ano=list()
-  
-  for (n in year_i:year_f){
+for (n in year_i:year_f){
     
-    clima_ano[[n-(year_i-1)]]=DATA[DATA$ano==n,1:9]
-    a=fechas_clima[n-(year_i-1),1]
-    b=paste(user,loc,"",".",a,sep="")
+    wth_year[[n-(year_i-1)]]=DATA[DATA$Year==n,1:9]
+    a=dates_wth[n-(year_i-1),1]
+    b=paste(getwd(),"//WTH/" ,local, 1, ".",a,sep="")
     sink(b)
-    cat(cord)
+    cat(set_head)
     cat("\n")
-    write.table(clima_ano[[n-(year_i-1)]],sep=",",row.names=F,col.names=F)
+    write.table(wth_year[[n-(year_i-1)]],sep=",",row.names=F,col.names=F)
     sink()
     
-  }
+}
+
+    
+}
+
+Make_WTH_ORYZA(DATA)
   
   
